a62a756d3d179480e5555235038711b9bf088af7
Marion Rose <marionrose@Marions-MacBook-Pro.local>
2022-11-02 14:40:26 -0500

    Added fetch and styled data

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'serif': ['Playfair Display', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
